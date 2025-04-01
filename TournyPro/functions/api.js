7e5aa7e568c9cfc6ec7d51b277977e5101ea1825
Mr Lunatic <mrlunatic.davis@gmail.com>
2025-04-01 21:02:10 +0300

    deleted:    TournyPro/functions/api.js 	deleted:    TournyPro/netlify.toml

--- TournyPro/functions/api.js
+++
@@ -1,8 +0,0 @@
-const { createRequestHandler } = require('@netlify/next');
-
-module.exports.handler = createRequestHandler({
-  // If you have custom Next.js configuration, you can provide it here
-  // next: {
-  //   compression: false,
-  // }
-});
